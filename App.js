8265a3e9183261705c170ca07bf9f9a2f3086f55
JeanSolo <jeanpksolo@gmail.com>
2022-06-13 13:47:21 -0400

    feat: add context provider and update render logic

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import AppNavigator from "./navigations/AppNavigator";
+import AuthNavigator from "./navigations/AuthNavigator";
+import React from "react";
+import { AuthProvider } from "./context/authcontext/AuthContext";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <AuthProvider>
+      <NavigationContainer>
+        <AuthNavigator />
+      </NavigationContainer>
+    </AuthProvider>
   );
 }
 
