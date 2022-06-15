6192c3fa4c2f1b4769d6d8146bbe6cc3931f996a
jadeleafmoon <jadeleafmoon@gmail.com>
2022-06-15 21:31:22 +0900

    feat: basic template for OtherUserProfileScreen

--- screens/user-profile/OtherUserProfileScreen.js
+++ screens/user-profile/OtherUserProfileScreen.js
@@ -2,17 +2,22 @@ import React from 'react';
 import { Card, Title, List, Button } from 'react-native-paper';
 import { Text, View, SafeAreaView, StyleSheet } from 'react-native';
 
-import { Text, StyleSheet, View } from 'react-native'
-import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native';
+import React, { Component } from 'react';
 
-export default class OtherUserProfileScreen extends Component {
-  render() {
-    return (
-      <View>
-        <Text>OtherUserProfileScreen</Text>
-      </View>
-    )
-  }
-}
+const OtherUserProfileScreen = () => {
+	const userData = {
+		username : 'KumikoKM',
+		age      : 28
+	};
 
-const styles = StyleSheet.create({})
+	return (
+		<SafeAreaView style={styles.root}>
+			<Text>Testing</Text>
+		</SafeAreaView>
+	);
+};
+
+export default OtherUserProfileScreen;
+
+const styles = StyleSheet.create({});
