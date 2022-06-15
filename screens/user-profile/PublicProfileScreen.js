608718e372797aa547520ad53bf337187f13189d
jadeleafmoon <jadeleafmoon@gmail.com>
2022-06-15 23:32:30 +0900

    fix: move code to PublicProfileScreen.js

--- screens/user-profile/PublicProfileScreen.js
+++ screens/user-profile/PublicProfileScreen.js
@@ -1,12 +1,21 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React from 'react';
+import { Text, View, SafeAreaView, StyleSheet } from 'react-native';
+import { Card, Title, List, Button } from "react-native-paper";
+
 
 const PublicProfileScreen = () => {
+  const userData = {
+		username : 'KumikoKM',
+		age      : 28
+	};
+
   return (
-    <View>
-      <Text>This is user's public profile</Text>
-    </View>
+		<SafeAreaView style={styles.root}>
+			<Text>Kumiko's Public Profile</Text>
+		</SafeAreaView>
   );
 };
 
 export default PublicProfileScreen;
+
+const styles = StyleSheet.create({});
