1698967b210ce6532487fae00283723353215c87
jadeleafmoon <jadeleafmoon@gmail.com>
2022-06-15 20:51:23 +0900

    feat: add OtherUserProfileScreen.js

---
+++ screens/user-profile/OtherUserProfileScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Card, Title, List, Button } from 'react-native-paper';
+import { Text, View, SafeAreaView, StyleSheet } from 'react-native';
+
+import { Text, StyleSheet, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class OtherUserProfileScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>OtherUserProfileScreen</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
