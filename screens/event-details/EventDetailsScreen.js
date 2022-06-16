8b2061bf82144c3f609d3c8595514f851bbc98db
JeanSolo <jeanpksolo@gmail.com>
2022-06-16 06:33:09 -0400

    chore: remove console.log

--- screens/event-details/EventDetailsScreen.js
+++ screens/event-details/EventDetailsScreen.js
@@ -13,10 +13,6 @@ import Color from "../../assets/themes/Color.js";
 import LongButton from "../../components/LongButton.js";
 
 const EventDetailsScreen = ({ navigation, eventData, data, setData }) => {
-  console.log("JESUS");
-  console.log("ED", eventData)
-  console.log("data", data)
-  
   const openCreatorProfile = () => {
     navigation.navigate("Creator Profile");
   };
