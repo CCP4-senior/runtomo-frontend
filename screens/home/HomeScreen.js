4375be8e7959c3ea7c38216640bb292818bb5677
jadeleafmoon <jadeleafmoon@gmail.com>
2022-07-01 16:32:44 +0900

    wip

--- screens/home/HomeScreen.js
+++ screens/home/HomeScreen.js
@@ -109,7 +109,7 @@ const HomeScreen = ({ navigation, /*data,*/ setCurrEvent }) => {
         </TouchableOpacity>
       </View>
 
-      {/* List of Run Events */}
+      {/* Wrapper for Run Events */}
 
       <View style={styles.container}>
         <FilterModal modalVisible={filterModalVisible} hideModal={hideModal} />
@@ -125,6 +125,8 @@ const HomeScreen = ({ navigation, /*data,*/ setCurrEvent }) => {
 };
 
 export default HomeScreen;
+
+// Component for listing Run Events
 
 const EventsDataPage = ({ selectEvent }) => {
   const {
