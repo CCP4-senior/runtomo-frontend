e81ffe2857d5403605794c651e64b5c2ca46260d
jadeleafmoon <jadeleafmoon@gmail.com>
2022-07-06 00:22:45 +0900

    fix: npm install

--- navigations/EventDetailsNavigator.js
+++ navigations/EventDetailsNavigator.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import EventDetailsScreen from "../screens/event-details/EventDetailsScreen";
-import PublicProfileScreen from "../screens/user-profile/PublicProfileScreen";
 import ConfirmationScreen from "../screens/confirmation/ConfirmationScreen";
 import HeaderStyle from "../assets/themes/HeaderStyle.js";
 import EventEditScreen from "../screens/event-details/EventEditScreen";
@@ -13,10 +12,6 @@ const EventDetailsNavigator = () => {
     <Stack.Navigator>
       <Stack.Screen name="Running Event" options={{ ...HeaderStyle }}>
         {(props) => <EventDetailsScreen {...props} />}
-      </Stack.Screen>
-
-      <Stack.Screen name="Creator Profile" options={{ ...HeaderStyle }}>
-        {(props) => <PublicProfileScreen {...props} />}
       </Stack.Screen>
 
       <Stack.Screen name="Event Joined" options={{ headerShown: false }}>
